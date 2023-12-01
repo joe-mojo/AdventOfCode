7526f96556e9dce537a191935be9be0e75fa258b
joe-mojo <joachim.e.rousseau+github@gmail.com>
2023-12-01 10:48:34 +0100

    WIP 2023 d1

---
+++ src/main/scala/org/jro/adventofcode/y2023/Day1.scala
@@ -0,0 +1,23 @@
+package org.jro.adventofcode.y2023
+
+/**
+ * @author joe_mojo.
+ *         2023/12/01
+ */
+object Day1 {
+	/*
+	SAMPLE:
+	-----------
+	1abc2
+	pqr3stu8vwx
+	a1b2c3d4e5f
+	treb7uchet
+	-----------
+	RESULT: 12 + 35 + 15 + 77 = 142
+	 */
+
+	def main(args: Array[String]): Unit = {
+
+	}
+
+}
