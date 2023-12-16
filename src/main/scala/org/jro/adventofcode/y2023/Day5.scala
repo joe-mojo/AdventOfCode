cb66a40ff5f6cbf96cccf4c7600d8299a0d3db01
joe-mojo <joachim.e.rousseau+github@gmail.com>
2023-12-16 20:21:19 +0100

    2023 d5 part 1

--- src/main/scala/org/jro/adventofcode/y2023/Day5.scala
+++ src/main/scala/org/jro/adventofcode/y2023/Day5.scala
@@ -4,6 +4,9 @@ import org.jro.adventofcode
 import org.jro.adventofcode.getInputLinesOrThrow
 import org.jro.adventofcode.y2023.Day5.*
 
+import scala.util.matching.Regex
+import scala.collection.mutable.ListBuffer
+
 case class Day5(seeds: Set[Int], steps: Seq[Step]) {
 
 }
@@ -12,27 +15,111 @@ object Day5 {
 
 	case class Step(mappings: Set[Mapping]) {
 		def map(srcValue: Long): Long = {
-			???
+			mappings.find(_.isDefinedAt(srcValue)).fold(srcValue)(_(srcValue))
 		}
 	}
-	case class Mapping(dstStart: Int, srcStart: Int, range: Long) extends PartialFunction[Long, Long] {
+	case class Mapping(dstStart: Long, srcStart: Long, range: Long) extends PartialFunction[Long, Long] {
 		val dstEnd: Long = dstStart + range - 1
 		val srcEnd: Long = srcStart + range - 1
 		val offset: Long = dstStart - srcStart
 
-		override def apply(srcValue: Long): Long = {
-			if(srcValue >= srcStart && srcValue <= srcEnd) srcValue + offset
-			else srcValue
-		}
-
+		override def apply(srcValue: Long): Long = srcValue + offset
 
 		override def isDefinedAt(value: Long): Boolean = {
-			???
+			value >= srcStart && value <= srcEnd
 		}
+	}
+	case class Input(seeds: Seq[Long], steps: Seq[Step])
+
+	def parseLines(lines: Iterable[String]): Input = {
+		val seeds = lines.head.split("""\s""").tail.map(_.toLong).toSeq
+		println(s"Parsed seeds: $seeds")
+
+		Input(seeds= seeds, steps = StepsParser.parseSteps(lines.tail))
+	}
+
+	object StepsParser {
+		val NewStep: Regex = """^[a-z- ]+map:$""".r
+		val stepsAccumulator: ListBuffer[Step] = ListBuffer.empty
+		val mappingAccumulator: ListBuffer[Mapping] = ListBuffer.empty
+
+		private def createStep(): Unit = {
+			if (mappingAccumulator.nonEmpty) {
+				stepsAccumulator += Step(mappingAccumulator.toSet)
+				mappingAccumulator.clear()
+			}
+		}
+
+		private def parseLine(line: String): Unit = {
+			line match
+				case "" => ()
+				case NewStep() =>
+					createStep()
+				case mappingString =>
+					val values = mappingString.split(" ").map(_.toLong)
+					mappingAccumulator += Mapping(values.head, values(1), values(2))
+		}
+
+		def parseSteps(lines: Iterable[String]): Seq[Step] = {
+			lines.foreach(parseLine)
+			createStep()
+			val res = stepsAccumulator.toIndexedSeq
+			stepsAccumulator.clear()
+			res
+		}
+
+	}
+
+	def puzzle1(lines: Iterable[String]): Long = {
+		val input: Input = parseLines(lines)
+		val locations: Seq[Long] = input.seeds.map { seed =>
+			val location = input.steps.foldLeft(seed) { (value, step) =>
+				step.map(value)
+			}
+			println(s"seed $seed --> location $location")
+			location
+		}
+		locations.min
 	}
 
 
+
 	def main(args: Array[String]): Unit = {
-		val lines = getInputLinesOrThrow(2023, 5)
+		val sample = """seeds: 79 14 55 13
+					   |
+					   |seed-to-soil map:
+					   |50 98 2
+					   |52 50 48
+					   |
+					   |soil-to-fertilizer map:
+					   |0 15 37
+					   |37 52 2
+					   |39 0 15
+					   |
+					   |fertilizer-to-water map:
+					   |49 53 8
+					   |0 11 42
+					   |42 0 7
+					   |57 7 4
+					   |
+					   |water-to-light map:
+					   |88 18 7
+					   |18 25 70
+					   |
+					   |light-to-temperature map:
+					   |45 77 23
+					   |81 45 19
+					   |68 64 13
+					   |
+					   |temperature-to-humidity map:
+					   |0 69 1
+					   |1 0 69
+					   |
+					   |humidity-to-location map:
+					   |60 56 37
+					   |56 93 4""".stripMargin.split("""\n""").toIndexedSeq
+		println(s"Puzzle1 with sample = ${puzzle1(sample)}")
+		val lines: Iterable[String] = getInputLinesOrThrow(2023, 5)
+		println(s"Puzzle1 with input = ${puzzle1(lines)}")
 	}
 }
--- src/test/scala/org/jro/adventofcode/y2023/Day5Spec.scala
+++ src/test/scala/org/jro/adventofcode/y2023/Day5Spec.scala
@@ -1,23 +1,52 @@
 package org.jro.adventofcode.y2023
 
-import org.jro.adventofcode.y2023.Day5.Mapping
+import org.jro.adventofcode.y2023.Day5.{Mapping, Step}
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 
 class Day5Spec extends AnyFreeSpec with Matchers {
 
-	"Day5 Mapping should map according to range" in {
-		val mapping = Mapping(50, 98, 2)
+	"Day5" - {
+		"Mapping should map according to range" in {
+			val mapping = Mapping(50, 98, 2)
 
-		mapping.apply(98) should be(50)
-		mapping.apply(99) should be(51)
-		for{i <- 0 to 97} {
-			mapping.apply(i) should be(i)
+			mapping.apply(98) should be(50)
+			mapping.apply(99) should be(51)
+			for {i <- 0 to 97} {
+				mapping.isDefinedAt(i) should be(false)
+			}
+			for {i <- 100 to 200} {
+				mapping.isDefinedAt(i) should be(false)
+			}
+
 		}
-		for {i <- 100 to 200} {
-			mapping.apply(i) should be(i)
+
+		"Step should map according to ranges" in {
+			val step = Step(Set(Mapping(50, 98, 2), Mapping(52, 50, 48)))
+
+			// no mapping from 0 to 49
+			for {i <- 0 until 50} {
+				step.map(i) should be(i)
+			}
+
+			// Mapping(52, 50, 48)
+			for {i <- 50 until (50 + 48)} {
+				step.map(i) should be(i+2)
+			}
+
+			// Mapping(50, 98, 2)
+			step.map(98) should be(50)
+			step.map(99) should be(51)
+
+			// no mapping from 100 to ...
+			for {i <- 100 until 200 } {
+				step.map(i) should be(i)
+			}
+
 		}
 
 	}
+
+
 
 }
